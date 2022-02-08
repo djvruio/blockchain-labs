c14b34ecfd33ee8055c344cd55f677f0de393b53
djvruio <djvruio@gmail.com>
2022-02-07 21:19:26 -0500

    rust: hello cargo program

---
+++ rust_projects/hello_cargo/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    println!("Hello, world from Cargo!");
+    println!("Cargo is a build system and a package manager!");
+}
+
+//cargo check
+//cargo build
+//cargo run
+//cargo new
+//cargo build --release
